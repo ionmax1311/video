bf4a51f43e45addc2fd9b245ff6150ed9fa42b08
student1311 <33418671+student1311@users.noreply.github.com>
2019-12-04 18:34:05 +0200

    Update send.php

--- send.php
+++ send.php
@@ -9,5 +9,5 @@ $ctx = stream_context_create(array(
         ) 
     ) 
 ); 
-file_get_contents('https://smsc.ru/sys/send.php?login=milestone&psw=dlkUY34R&phones=' .$phone.'&mes='.$res.'-ваш код подтверждения на start.finstudy'.'&charset=utf-8', 0, $ctx); 
+file_get_contents('https://smsc.ru/sys/send.php?login=********&psw=********&phones=' .$phone.'&mes='.$res.'-ваш код подтверждения на start.finstudy'.'&charset=utf-8', 0, $ctx); 
 ?>
